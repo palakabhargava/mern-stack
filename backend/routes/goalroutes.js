84035043c38142fcdbc9b7a6ce29c584577cc200
bhargav354 <palakabhargava34@gmail.com>
2026-06-06 19:01:05 +0530

    goals controller

---
+++ backend/routes/goalroutes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const router = express.Router();
+const { getGoals,setGoal,updateGoal,deleteGoal } = require('../controllers/goalcontroller');
+router.route('/').get(getGoals).post(setGoal);
+router.route('/:id').put(updateGoal).delete(deleteGoal);
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log("Hello World!");
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port= process.env.PORT || 5000;
+const { errorHandler } = require('./middleware/errormiddleware');
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
+
+
+app.use('/api/goals', require('./routes/goalroutes'));
+app.use(errorHandler);
+
+app.listen(port,()=>{
+    console.log(`server is running on port ${port}`);
+})
